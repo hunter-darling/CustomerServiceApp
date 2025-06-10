5beb37092789e9c99e89be2bf2cbad6926c663d0
Hunter Darling <hunterperryd@gmail.com>
2025-06-10 17:47:49 -0400

    fix tests, code cleanup

--- src/test/java/org/hpd/customerservice/mapper/CustomerMapperTest.java
+++ src/test/java/org/hpd/customerservice/mapper/CustomerMapperTest.java
@@ -8,35 +8,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerMapperTest {
-    private CustomerMapper customerMapper;
+  private CustomerMapper customerMapper;
 
-    @BeforeEach
-    void setUp() {
-        customerMapper = new CustomerMapper();
-    }
+  @BeforeEach
+  void setUp() {
+    customerMapper = new CustomerMapper();
+  }
 
-    @Test
-    void testTranslateToEntity_Success() {
-        CreateCustomerRequest createCustomerRequest = new CreateCustomerRequest();
-        createCustomerRequest.setFirstName("John");
-        createCustomerRequest.setLastName("Doe");
-        createCustomerRequest.setEmailAddress("john.doe@example.com");
-        createCustomerRequest.setAddress("123 Main St");
-        createCustomerRequest.setCity("Test City");
-        createCustomerRequest.setState("Test State");
-        createCustomerRequest.setZip("12345");
+  @Test
+  void testTranslateToEntity_Success() {
+    CreateCustomerRequest createCustomerRequest = new CreateCustomerRequest();
+    createCustomerRequest.setFirstName("John");
+    createCustomerRequest.setLastName("Doe");
+    createCustomerRequest.setEmailAddress("john.doe@example.com");
+    createCustomerRequest.setAddress("123 Main St");
+    createCustomerRequest.setCity("Test City");
+    createCustomerRequest.setState("Test State");
+    createCustomerRequest.setZip("12345");
 
-        CustomerEntity expectedCustomerEntity = new CustomerEntity(null,
-                "John",
-                "Doe",
-                "john.doe@example.com",
-                "123 Main St",
-                "Test City",
-                "Test State",
-                "12345"
-        );
+    CustomerEntity expectedCustomerEntity = new CustomerEntity(
+        "John",
+        "Doe",
+        "john.doe@example.com",
+        "123 Main St",
+        "Test City",
+        "Test State",
+        "12345");
 
-        CustomerEntity actualCustomerEntity = customerMapper.translateToEntity(createCustomerRequest);
-        assertEquals(expectedCustomerEntity, actualCustomerEntity);
-    }
+    CustomerEntity actualCustomerEntity = customerMapper.translateToEntity(createCustomerRequest);
+    assertEquals(expectedCustomerEntity, actualCustomerEntity);
+  }
 }
