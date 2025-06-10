1d5a78f509084a7920452be458c142f4ab085d4c
Hunter Darling <hunterperryd@gmail.com>
2025-06-10 17:27:31 -0400

    remove create request field validation, whitespace cleanup

--- src/main/java/org/hpd/customerservice/entity/CustomerEntity.java
+++ src/main/java/org/hpd/customerservice/entity/CustomerEntity.java
@@ -20,52 +20,54 @@ import java.util.Objects;
 @NoArgsConstructor
 public class CustomerEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "customer_id")
-    private Long customerId;
-    @Column(name = "first_name", length = 50)
-    @NotEmpty
-    private String firstName;
-    @Column(name = "last_name", length = 50)
-    private String lastName;
-    @Column(name = "email_address", length = 100)
-    private String emailAddress;
-    @Column(name = "address", length = 225)
-    private String address;
-    @Column(name = "city", length = 50)
-    private String city;
-    @Column(name = "state", length = 25)
-    private String state;
-    @Column(name = "zip", length = 10)
-    private String zip;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "customer_id")
+  private Long customerId;
+  @Column(name = "first_name", length = 50)
+  @NotEmpty
+  private String firstName;
+  @Column(name = "last_name", length = 50)
+  private String lastName;
+  @Column(name = "email_address", length = 100)
+  private String emailAddress;
+  @Column(name = "address", length = 225)
+  private String address;
+  @Column(name = "city", length = 50)
+  private String city;
+  @Column(name = "state", length = 25)
+  private String state;
+  @Column(name = "zip", length = 10)
+  private String zip;
 
-    public CustomerEntity(Long id, String firstName, String lastName, String emailAddress, String address, String city, String state, String zip) {
-        if (firstName == null || lastName == null || emailAddress == null || city == null
-                || firstName.isEmpty() || lastName.isEmpty() || emailAddress.isEmpty() || city.isEmpty()) {
-            throw new IllegalArgumentException();
-        } else {
-            this.customerId = id;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.emailAddress = emailAddress;
-            this.address = address;
-            this.city = city;
-            this.state = state;
-            this.zip = zip;
-        }
-    }
+  public CustomerEntity(String firstName, String lastName, String emailAddress, String address, String city,
+      String state, String zip) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emailAddress = emailAddress;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CustomerEntity that = (CustomerEntity) o;
-        return Objects.equals(customerId, that.customerId) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(emailAddress, that.emailAddress) && Objects.equals(address, that.address) && Objects.equals(city, that.city) && Objects.equals(state, that.state) && Objects.equals(zip, that.zip);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(customerId, firstName, lastName, emailAddress, address, city, state, zip);
-    }
+    CustomerEntity that = (CustomerEntity) o;
+
+    return Objects.equals(customerId, that.customerId) && Objects.equals(firstName, that.firstName)
+        && Objects.equals(lastName, that.lastName) && Objects.equals(emailAddress, that.emailAddress)
+        && Objects.equals(address, that.address) && Objects.equals(city, that.city) && Objects.equals(state, that.state)
+        && Objects.equals(zip, that.zip);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(customerId, firstName, lastName, emailAddress, address, city, state, zip);
+  }
 }
