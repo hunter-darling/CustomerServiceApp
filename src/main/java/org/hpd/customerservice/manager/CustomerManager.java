c810359133dd2bc9be23a4408bd25ff8cc2e64ae
Hunter Darling <hunterperryd@gmail.com>
2025-06-10 17:28:25 -0400

    code cleanup

--- src/main/java/org/hpd/customerservice/manager/CustomerManager.java
+++ src/main/java/org/hpd/customerservice/manager/CustomerManager.java
@@ -13,43 +13,45 @@ import java.util.List;
 @Component
 public class CustomerManager {
 
-    CustomerRepository customerRepository;
-    CustomerMapper customerMapper;
+  CustomerRepository customerRepository;
+  CustomerMapper customerMapper;
 
-    public CustomerManager(CustomerRepository customerRepository, CustomerMapper customerMapper) {
-        this.customerRepository = customerRepository;
-        this.customerMapper = customerMapper;
-    }
+  public CustomerManager(CustomerRepository customerRepository, CustomerMapper customerMapper) {
+    this.customerRepository = customerRepository;
+    this.customerMapper = customerMapper;
+  }
 
-    public List<CustomerEntity> getAllCustomers() throws SQLException {
-        List<CustomerEntity> customerEntities = this.customerRepository.findAll();
-        if (customerEntities == null || customerEntities.isEmpty()) {
-            throw new SQLException();
-        } else {
-            return customerEntities;
-        }
+  public List<CustomerEntity> getAllCustomers() throws SQLException {
+    List<CustomerEntity> customerEntities = this.customerRepository.findAll();
+    if (customerEntities == null || customerEntities.isEmpty()) {
+      throw new SQLException();
+    } else {
+      return customerEntities;
     }
+  }
 
-    public List<CustomerEntity> getCustomersByCity(String city) throws SQLException {
-        List<CustomerEntity> customerEntities = this.customerRepository.findByCity(city);
-        if (customerEntities == null || customerEntities.isEmpty()) {
-            throw new SQLException();
-        } else {
-            return customerEntities;
-        }
+  public List<CustomerEntity> getCustomersByCity(String city) throws SQLException {
+    List<CustomerEntity> customerEntities = this.customerRepository.findByCity(city);
+    if (customerEntities == null || customerEntities.isEmpty()) {
+      throw new SQLException();
+    } else {
+      return customerEntities;
     }
+  }
 
-    public CustomerEntity getCustomerById(Long customerId) throws SQLException {
-        CustomerEntity customerEntity = this.customerRepository.findById(customerId).orElse(null);
-        if (customerEntity == null) {
-            throw new SQLException();
-        } else {
-            return customerEntity;
-        }
+  public CustomerEntity getCustomerById(Long customerId) throws SQLException {
+    CustomerEntity customerEntity = this.customerRepository.findById(customerId).orElse(null);
+    if (customerEntity == null) {
+      throw new SQLException();
+    } else {
+      return customerEntity;
     }
+  }
 
-    public CustomerEntity addNewCustomer(CreateCustomerRequest createCustomerRequest) throws DataIntegrityViolationException {
-        CustomerEntity customerEntity = this.customerMapper.translateToEntity(createCustomerRequest);
-        return this.customerRepository.save(customerEntity);
-    }
+  public CustomerEntity addNewCustomer(CreateCustomerRequest createCustomerRequest)
+      throws DataIntegrityViolationException {
+
+    CustomerEntity customerEntity = this.customerMapper.translateToEntity(createCustomerRequest);
+    return this.customerRepository.save(customerEntity);
+  }
 }
