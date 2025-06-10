f60f3c6027daa131b98399417e4b374ac211cf13
Hunter Darling <hunterperryd@gmail.com>
2025-06-10 17:44:22 -0400

    add @NonNull annotation to findAll() method (Spring JPA findAll() i snever null)

--- src/main/java/org/hpd/customerservice/repository/CustomerRepository.java
+++ src/main/java/org/hpd/customerservice/repository/CustomerRepository.java
@@ -2,12 +2,14 @@ package org.hpd.customerservice.repository;
 
 import org.hpd.customerservice.entity.CustomerEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.List;
 
 public interface CustomerRepository extends CrudRepository<CustomerEntity, Long> {
 
-    List<CustomerEntity> findAll();
+  @NonNull
+  List<CustomerEntity> findAll();
 
-    List<CustomerEntity> findByCity(String city);
+  List<CustomerEntity> findByCity(String city);
 }
